3d653768bcb381480ed0e7e623ea008bd37eed68
arsildo <arsildo.murati@gmail.com>
2022-09-20 22:20:54 +0200

    added landing screen

--- app/src/main/java/com/arsildo/merr_patenten/display/screens/MainScreen.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/screens/MainScreen.kt
@@ -1,9 +1,22 @@
 package com.arsildo.merr_patenten.display.screens
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.arsildo.merr_patenten.display.activities.ExamActivity
+import com.arsildo.merr_patenten.display.screens.components.ScreenLayout
+import com.arsildo.merr_patenten.logic.extensions.launchActivity
 
 @Composable
 fun MainScreen() {
-    Text(text = "Main Screen")
+    ScreenLayout {
+        val context = LocalContext.current
+        Button(
+            onClick = { context.launchActivity<ExamActivity>() }
+        ) {
+            Text(text = "Navigate")
+        }
+    }
+    //TODO Fill Display
 }
