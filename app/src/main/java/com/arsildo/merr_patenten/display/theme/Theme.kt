55a8b13232aa0014b53c5c52782e07dcb3c9875f
arsildo <arsildo.murati@gmail.com>
2022-09-20 22:20:33 +0200

    implemented system ui controller

--- app/src/main/java/com/arsildo/merr_patenten/display/theme/Theme.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/theme/Theme.kt
@@ -5,33 +5,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = White // TODO CHANGE
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    secondary = Teal200,
+    background = White
 )
 
 @Composable
 fun MerrPatentenTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
     val colors = if (darkTheme) {
+        systemUiController.setNavigationBarColor(MaterialTheme.colors.background)
+        systemUiController.setStatusBarColor(MaterialTheme.colors.background)
         DarkColorPalette
     } else {
+        systemUiController.setNavigationBarColor(MaterialTheme.colors.background)
+        systemUiController.setStatusBarColor(MaterialTheme.colors.background)
         LightColorPalette
     }
 
