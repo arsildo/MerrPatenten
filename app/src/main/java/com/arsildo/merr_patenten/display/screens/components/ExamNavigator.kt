ad7e858636fc1e7801bd3ab5e56a2d04b41acb46
arsildo <arsildo.murati@gmail.com>
2022-09-22 14:18:15 +0200

    added text, buttons and weights

--- app/src/main/java/com/arsildo/merr_patenten/display/screens/components/ExamNavigator.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/screens/components/ExamNavigator.kt
@@ -3,7 +3,12 @@ package com.arsildo.merr_patenten.display.screens.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Map
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -11,13 +16,34 @@ import androidx.compose.ui.Modifier
 @Composable
 fun ExamNavigator(
     countDownTimer: MutableState<String>,
+    onClick: () -> Unit,
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceAround
     ) {
-        Text(text = "3/40")
-        Text(text = "Map")
-        Text(text = countDownTimer.value)
+        Text(text = "1/40", modifier = Modifier.weight(.3f))
+        IconButton(
+            onClick = onClick,
+            modifier = Modifier.weight(.3f)
+        ) {
+            Icon(
+                Icons.Rounded.Map, // TODO FIX
+                contentDescription = null
+            )
+        }
+        Text(
+            text = countDownTimer.value,
+            modifier = Modifier.weight(.3f)
+        )
+        IconButton(
+            onClick = onClick,
+            modifier = Modifier.weight(.1f)
+        ) {
+            Icon(
+                Icons.Rounded.Close, // TODO Implement Functionality and Animation
+                contentDescription = null,
+            )
+        }
     }
 }
