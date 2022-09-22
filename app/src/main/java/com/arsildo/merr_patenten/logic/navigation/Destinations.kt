62ce0fdb55ed5c05c871eec1f05846f361a2d889
arsildo <arsildo.murati@gmail.com>
2022-09-22 14:18:38 +0200

    added exam destination

--- app/src/main/java/com/arsildo/merr_patenten/logic/navigation/Destinations.kt
+++ app/src/main/java/com/arsildo/merr_patenten/logic/navigation/Destinations.kt
@@ -2,6 +2,7 @@ package com.arsildo.merr_patenten.logic.navigation
 
 sealed class Destinations(val route: String) {
     object Main : Destinations(route = "main_screen")
+    object Exam : Destinations(route = "exam_screen")
     object Preferences : Destinations(route = "preferences_screen")
     object Statistics : Destinations(route = "statistics_screen")
 }
