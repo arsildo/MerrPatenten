0c7aa7cd9359cd25b297e8d81186a6e3a610cf23
arsildo <arsildo.murati@gmail.com>
2022-09-22 14:16:34 +0200

    added basic legend buttons but no functionality

--- app/src/main/java/com/arsildo/merr_patenten/display/screens/components/PagerMap.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/screens/components/PagerMap.kt
@@ -1,32 +1,50 @@
 package com.arsildo.merr_patenten.display.screens.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PagerMap(
+    isExamCompleted: MutableState<Boolean>,
     sheetState: ModalBottomSheetState,
+    onPositionClicked: () -> Unit,
     onHideSheet: () -> Unit,
 ) {
     ModalBottomSheetLayout(
         sheetState = sheetState,
+        sheetBackgroundColor = MaterialTheme.colors.background,
+        sheetElevation = 0.dp,
         sheetContent = {
-            Column(Modifier.fillMaxWidth()) {
-                Text(text = "Bottom Sheet")
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "${isExamCompleted.value}")
 
-                LazyVerticalGrid(columns = GridCells.Fixed(count = 2)) {
-                    items(10){
-                        Text(text = "$it")
+                LazyVerticalGrid(columns = GridCells.Fixed(count = 5)) {
+                    items(40) {
+                        MapItem(
+                            position = "$it",
+                            onPositionClick = onPositionClicked // TODO Pas Position
+                        )
                     }
                 }
 
@@ -36,4 +54,20 @@ fun PagerMap(
             }
         }
     ) {}
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MapItem(
+    position: String,
+    onPositionClick: () -> Unit,
+) {
+    Card(
+        onClick = onPositionClick,
+    ) {
+        Text(
+            text = position,
+            textAlign = TextAlign.Center,
+        )
+    }
 }
