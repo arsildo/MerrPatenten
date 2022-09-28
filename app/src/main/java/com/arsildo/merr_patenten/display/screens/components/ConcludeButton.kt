cdf0d2bd35d0ab212df4e5f3f07819c61d86d357
arsildo <arsildo.murati@gmail.com>
2022-09-28 18:32:03 +0200

    fixed behaviour bug

--- app/src/main/java/com/arsildo/merr_patenten/display/screens/components/ConcludeButton.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/screens/components/ConcludeButton.kt
@@ -1,5 +1,6 @@
 package com.arsildo.merr_patenten.display.screens.components
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -9,7 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DoneAll
-import androidx.compose.material.icons.rounded.ExpandLess
+import androidx.compose.material.icons.rounded.Map
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,21 +26,26 @@ fun ConcludeButton(
 ) {
     var confirm by remember { mutableStateOf(2) }
     Button(
-        onClick = { if (confirm == 1) onClick() else confirm-- },
+        onClick = {
+            if (isExamCompleted) onClick() else confirm--; if (confirm == 0) onClick()
+        },
         colors = ButtonDefaults.buttonColors(
             backgroundColor = if (!isExamCompleted) MaterialTheme.colors.error else MaterialTheme.colors.primary,
             contentColor = MaterialTheme.colors.onPrimary,
         ),
         shape = MaterialTheme.shapes.medium,
-        elevation = ButtonDefaults.elevation(defaultElevation = 2.dp, pressedElevation = 4.dp),
+        elevation = ButtonDefaults.elevation(defaultElevation = 0.dp, pressedElevation = 0.dp),
+        contentPadding = PaddingValues(8.dp),
         modifier = Modifier.fillMaxWidth()
     ) {
         Icon(
-            if (!isExamCompleted) Icons.Rounded.DoneAll else Icons.Rounded.ExpandLess,
+            if (!isExamCompleted) Icons.Rounded.DoneAll else Icons.Rounded.Map,
             contentDescription = null,
             modifier = Modifier.padding(end = 8.dp)
         )
-        Text(text = if (!isExamCompleted) "Perfundo Provimin($confirm)" else "Shiko Gabimet")
+        Text(
+            text = if (!isExamCompleted) "Perfundo Provimin ($confirm)" else "Shiko Gabimet",
+        )
     }
 
 }
