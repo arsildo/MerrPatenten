070b2c1db95469f8804e20890dfd2f5a8e7a8a23
arsildo <arsildo.murati@gmail.com>
2022-09-29 21:08:04 +0200

    added additional info

--- app/src/main/java/com/arsildo/merr_patenten/display/screens/MainScreen.kt
+++ app/src/main/java/com/arsildo/merr_patenten/display/screens/MainScreen.kt
@@ -1,9 +1,14 @@
 package com.arsildo.merr_patenten.display.screens
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -14,10 +19,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DirectionsCar
+import androidx.compose.material.icons.rounded.ExpandMore
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.rounded.SportsScore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -51,12 +63,23 @@ fun CategoryCard(onStartExamClicked: () -> Unit) {
         elevation = 2.dp,
     ) {
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .animateContentSize(
+                    animationSpec = tween(
+                        delayMillis = 64,
+                        durationMillis = 128,
+                        easing = LinearEasing
+                    )
+                ),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Category", fontSize = 32.sp)
+            Text(text = "A1, A2, B1, B", fontSize = 32.sp, color = MaterialTheme.colors.primary)
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
@@ -65,23 +88,54 @@ fun CategoryCard(onStartExamClicked: () -> Unit) {
                     contentDescription = null,
                     modifier = Modifier.size(128.dp)
                 )
-                Column() {
-                    Text(text = "1")
-                    Text(text = "2")
-                    Text(text = "3")
-                    Text(text = "4")
+                Column(
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(text = "Versioni 2022")
+                    Text(text = "40 Pyetje")
+                    Text(text = "40 Minuta")
+                    Text(text = "4 Gabime te lejuara")
                 }
             }
-            Button(
-                onClick = onStartExamClicked,
-                colors = ButtonDefaults.buttonColors(
-                    contentColor = MaterialTheme.colors.onPrimary,
-                    backgroundColor = MaterialTheme.colors.primary,
-                ),
-                shape = MaterialTheme.shapes.medium,
-                elevation = ButtonDefaults.elevation(0.dp)
+            var additionalInfo by remember { mutableStateOf(false) }
+            Row(
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = "Start")
+                Button(
+                    onClick = onStartExamClicked,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colors.onPrimary,
+                        backgroundColor = MaterialTheme.colors.primary,
+                    ),
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 0.dp,
+                    ),
+                    modifier = Modifier.fillMaxWidth(.85f)
+                ) {
+                    Icon(
+                        Icons.Rounded.SportsScore,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                    Text(text = "Fillo Provimin")
+                }
+                IconButton(
+                    onClick = { additionalInfo = !additionalInfo },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    val rotationState by animateFloatAsState(if (additionalInfo) 180f else 0f)
+                    Icon(
+                        Icons.Rounded.ExpandMore,
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.primary,
+                        modifier = Modifier.rotate(rotationState)
+                    )
+                }
+            }
+            if (additionalInfo) {
+                Text(text = "additional info")
             }
         }
     }
