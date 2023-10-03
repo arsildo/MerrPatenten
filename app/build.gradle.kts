47e3aefdbda028ebcd1a39ddd59a8d9df46629d1
arsildo <arsildo.murati@gmail.com>
2023-10-03 19:50:20 +0200

    updated dependencies and version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.arsildo.merrpatenten"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
@@ -84,6 +84,6 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.timber)
 
+    implementation(platform(libs.koin.bom))
     implementation(libs.bundles.koin)
-    ksp(libs.koin.ksp)
 }
