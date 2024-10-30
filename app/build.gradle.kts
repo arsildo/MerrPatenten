227740a23f50bd483c2afea4d0b04f115a058ac1
arsildo <arsildo.murati@gmail.com>
2024-10-30 17:35:14 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.arsildo.merrpatenten"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 30
-        versionName = "2.3.0"
+        versionCode = 31
+        versionName = "2.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
