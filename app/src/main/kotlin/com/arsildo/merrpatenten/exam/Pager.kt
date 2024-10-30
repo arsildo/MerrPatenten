751c7c54c9342c1ec1b012ca69b13c8047b8e5dd
arsildo <arsildo.murati@gmail.com>
2024-10-30 17:23:05 +0100

    update dependencies

--- app/src/main/kotlin/com/arsildo/merrpatenten/exam/Pager.kt
+++ app/src/main/kotlin/com/arsildo/merrpatenten/exam/Pager.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,7 +18,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -43,7 +41,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -56,7 +53,6 @@ import com.arsildo.merrpatenten.utils.formatQuestion
 import com.arsildo.merrpatenten.utils.getImageResource
 
 @Suppress("MagicNumber")
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Pager(
     questions: List<Question>,
